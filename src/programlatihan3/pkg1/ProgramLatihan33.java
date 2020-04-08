f572c787396fde27782ee8dbde6a29193f2c4649
Ilham Eko Aris Saputro <62685938+ilhamcool@users.noreply.github.com>
2020-04-08 21:17:56 +0700

    Add files via upload

---
+++ src/programlatihan3/pkg1/ProgramLatihan33.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programlatihan3.pkg1;
+
+/**
+ *
+ * @author rayaz
+ */
+public class ProgramLatihan33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        latihan33 lat = new latihan33();
+        
+       lat.setVisible(true);
+    }
+    
+}
